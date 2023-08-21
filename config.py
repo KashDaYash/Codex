7d71eb71937063046ad37e8a4f7dc3efea3c6215
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2023-08-21 22:00:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "12124605"))
-API_HASH = os.getenv("API_HASH", "5cf3577d85fd02286535ec2296934287")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6262376276:AAHazSHPyVvCvGfpnZ2wDYRoYJp8F4O0yhY")
-OWNER_ID = int(os.getenv("OWNER_ID", "5558463511"))
-SESSION = os.getenv("SESSION", "BQG92WIARimkBvSWMxKS5BfzSFpZm4c7mOoHGu2C5iURAXk3u2cxhERs5UAOWmTNj6yJyc0W_ymq44fliMQUflwAI4HgDY2oTK2-BsdMBRqo-O97ixXfH_wxRZOMXsuxHZHa39vwlQYvsqg9eULdvjHuOqzDegqHY7Nz4UUNKm6b3-8lsH6YQHCt4eokbAypt9qH5J2HDv8Iz4RrNVo7MPHB7HbIfP1jJUBp08MH5zQgNd_D0Uald-pGccI99cscers-gUhdAnnqPnxmBQ1suIdX9hlJrJsBcIUBAAMsqnEJgOgbOC6kqIKw5EhpmefHCApDaYlWtcBbu8zjbTsULfnMRwBiKwAAAAE5qYYzAA")
-DATABASE_URI = os.getenv('DATABASE_URI', "mongodb+srv://Sohanrazz:Sohanrazz@cluster0.xsdpzcf.mongodb.net/?retryWrites=true&w=majority")
-OWNER = os.getenv("OWNER", "owner_21")
-LOG_CHANNEL = os.getenv("LOG_CHANNEL", "-1001697367157")
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+OWNER_ID = int(os.getenv("OWNER_ID", ""))
+SESSION = os.getenv("SESSION", "")
+DATABASE_URI = os.getenv('DATABASE_URI', "")
+OWNER = os.getenv("OWNER", "")
+LOG_CHANNEL = os.getenv("LOG_CHANNEL", "")
