cd03e0e69a6a563fb7e68dd693b9659765f6a2a1
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2023-08-22 19:32:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ class Bot(Client):
         try:
             await super().start()
             await YaaraOP.start()
-            await YaaraOP.send_message(ADMIN,"STARTED")# Start the User client
+            await YaaraOP.send_message("me,"STARTED")# Start the User client
             LOGGER.info("Bot Started ⚡")
         except Exception as e:
             LOGGER.exception("Error while starting bot: %s", str(e))
