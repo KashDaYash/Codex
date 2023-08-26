5741e4bb94862af5826740cfd4fdc4185ffa33dd
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-26 20:42:33 +0530

    gud

--- plugins/start.py
+++ plugins/start.py
@@ -6,7 +6,7 @@ from pyrogram.types import *
 
 
 
-@Client.on_message(filters.command("start"))
+@Client.on_message(filters.command("start") & filters.private & filters.group)
 async def start_handle(_, m):
   user = m.from_user
   id = m.chat.id
