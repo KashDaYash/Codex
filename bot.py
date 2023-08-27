439057e62d7575c6f4facd36652e94dafdf51477
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-27 10:33:14 +0530

    ho gya

--- bot.py
+++ bot.py
@@ -39,5 +39,5 @@ async def main():
     LOGGER.exception("Error while starting bot: %s", str(e))
 
 loop = asyncio.get_event_loop()
-loop.run_untile_complete(main())
+loop.run_forever(main())
 print("Bot Started 💥")
